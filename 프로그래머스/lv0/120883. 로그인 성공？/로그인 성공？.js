338a38d00c762efbd73939e563e3aea39db6e15e
밍갱 <84396795+min0312@users.noreply.github.com>
2023-04-09 15:15:25 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,17 @@
+function solution(id_pw, db) {
+    var answer = '';
+    
+    for (let i=0; i<db.length; i++) {
+        if (id_pw[0] === db[i][0]) {
+            if (id_pw[1] === db[i][1]) {
+                answer = "login"
+            } else {
+                answer = "wrong pw"
+            } break
+        } else {
+            answer = "fail"
+        }
+    }
+    
+    return answer;
+}
