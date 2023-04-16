a2e1f5d62532dde58b7a358e31a38e79ecaa080f
밍갱 <84396795+min0312@users.noreply.github.com>
2023-04-16 22:06:01 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.40 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,17 @@
+function solution(my_string) {
+    var answer = '';
+    
+    let arr = [];
+
+    for (let i = 0; i < my_string.length; i++) {
+        if (my_string[i] === my_string[i].toUpperCase()) { 
+            arr.push(my_string[i].toLowerCase());
+        } else {
+            arr.push(my_string[i].toUpperCase());
+        }
+    }
+
+    answer = arr.join("");
+    
+    return answer;
+}
