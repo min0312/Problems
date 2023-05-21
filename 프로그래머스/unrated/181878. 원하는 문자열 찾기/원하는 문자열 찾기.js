28d9228f5a4bc6e0cc61ae986c17c85428fa8f85
밍갱 <84396795+min0312@users.noreply.github.com>
2023-05-21 19:31:01 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 0.19 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -0,0 +1,12 @@
+function solution(myString, pat) {
+    var answer = 0;
+    
+    myString = myString.toLowerCase()
+    pat = pat.toLowerCase()
+    
+    if (myString.includes(pat)) {
+        answer = 1
+    }
+    
+    return answer;
+}
